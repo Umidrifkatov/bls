17f731785b209bb3c26745026274e94ba8385dc2
Umid <umidrifkatov@gmail.com>
2023-04-08 01:09:33 +0300

    r

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = '/static/'
 #     BASE_DIR / "static",
 #     # BASE_DIR / "staticfiles"
 # ]
-STATIC_ROOT = "static"
+STATIC_ROOT = BASE_DIR / "static",
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
